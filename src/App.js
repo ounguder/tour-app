6e62f0ac096a48158d403a348ac512585e2e884d
ounguder <ounguder@gmail.com>
2022-07-26 15:57:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import React, { useState, useEffect } from "react";
+import Tours from "./Tours";
+import { toBeRequired } from "@testing-library/jest-dom/dist/matchers";
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [tourList, setTourList] = useState([]);
+
+  const dataHandler = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(url);
+      const dataList = await response.json();
+      setTourList(dataList);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const removeItem = (id) => {
+    const newList = tourList.filter((tour) => tour.id !== id);
+    setTourList(newList);
+  };
+
+  useState(() => {
+    dataHandler();
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+        <h1>Loading...</h1>
+      </>
+    );
+  }
+
+  if (tourList.length === 0) {
+    return (
+      <>
+        <div className="title">
+          <h3>There is no Tour Left</h3>
+          <button className="btn" onClick={dataHandler}>
+            Refresh
+          </button>
+        </div>
+      </>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <Tours tourList={tourList} removeItem={removeItem} />
+      </main>
+    </>
   );
 }
 
---
+++ src/Tour.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const Tour = ({ id, name, info, image, price, removeItem }) => {
+  const [readMore, setReadMore] = useState(false);
+  return (
+    <>
+      <article className="single-tour">
+        <img src={image} alt={name} />
+
+        <footer>
+          <div className="tour-info">
+            <h4>{name}</h4>
+            <h4 className="tour-price">${price}</h4>
+          </div>
+          <p>
+            {readMore ? info : `${info.substring(0, 200)}...`}
+
+            <button onClick={() => setReadMore(!readMore)}>
+              {readMore ? `Read Less` : "Read More"}
+            </button>
+          </p>
+
+          <button className="delete-btn" onClick={() => removeItem(id)}>
+            Not Interested
+          </button>
+        </footer>
+      </article>
+    </>
+  );
+};
+export default Tour;
